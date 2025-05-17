d128da3820a709603c58bd6c3514a355ff99bdbb
atompsv <tomtomtom.tt953@gmail.com>
2025-05-17 16:47:16 +0700

    Fix manifest file

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,30 +1,24 @@
 import { NextResponse } from "next/server";
-import { APP_URL } from "../../../lib/constants";
+// import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-    accountAssociation: {
-      header:
-        "eyJmaWQiOjE3OTc5LCJ0eXBlIjoiY3VzdG9keSIsImtleSI6IjB4MGMxNWE5QkVmRTg3RjY0N0IwMDNhMjI0MTY4NDYwMzYyODQ0M2Y4YiJ9",
-      payload:
-        "eyJkb21haW4iOiJtb25hZC1taW5pYXBwLXRlbXBsYXRlLXNldmVuLnZlcmNlbC5hcHAifQ",
-      signature:
-        "MHgwYzY2NDdjZDhjOWJiY2JmYzg2NGIzZjVjYWVjY2ExMTdlOTY4ZGQwMWIzMmM0NGViMjU5ZDhlOGQyMzdhZTZiMDU1MmNmNWRiMDU1MDMwNTZmNTNhZmEwZDZlZTBlZmIyMmJmNDNmMDQ4NTdhMzk2NmY0YmMzODk2N2NlZDI5ZjFi",
+    "accountAssociation": {
+      "header": "eyJmaWQiOjU3Nzc4MywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDk2ODg4QjExZTEwOTdEMTdiQzk2MDRCYTdiODQ5Nzc5NDYxYUNCMDIifQ",
+      "payload": "eyJkb21haW4iOiJ3ZWIzLW1hcmtldHBsYWNlLmNvbSJ9",
+      "signature": "MHg4NjdmYjU3ZDhmZDdhN2MyNjY4Yzk0OWZmNDhmOGI1MmQxZjAzM2FhODQwMGVkMDZkOTUzMjY2ODA4ZjUyY2ZiNjVmOTUwYWRjOGNmOTRkODQzNjM3Yzg1NzZlYmE1NTIwNTVmNGNmZjhjM2RmZDI4MjBlNzdjNjg1MmZlYmRiODFi"
     },
-    frame: {
-      version: "1",
-      name: "web3-marketplace-miniapp",
-      iconUrl: `${APP_URL}/images/icon.png`,
-      homeUrl: `${APP_URL}`,
-      imageUrl: `${APP_URL}/images/feed.png`,
-      screenshotUrls: [],
-      tags: ["monad", "farcaster", "miniapp", "template", "ecommerce"],
-      primaryCategory: "developer-tools",
-      buttonTitle: "Launch Web3 Marketplace",
-      splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: "#ffffff",
-      webhookUrl: `${APP_URL}/api/webhook`,
-    },
+    "frame": {
+      "version": "1",
+      "name": "Example Frame",
+      "iconUrl": "https://web3-marketplace.com/icon.png",
+      "homeUrl": "https://web3-marketplace.com",
+      "imageUrl": "https://web3-marketplace.com/image.png",
+      "buttonTitle": "Check this out",
+      "splashImageUrl": "https://web3-marketplace.com/splash.png",
+      "splashBackgroundColor": "#eeccff",
+      "webhookUrl": "https://web3-marketplace.com/api/webhook"
+    }
   };
 
   return NextResponse.json(farcasterConfig);
